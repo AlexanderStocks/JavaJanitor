a68cfc3f3a088a1ecb7fad1fed8fb4560148e6f3
AlexStocks <alex.stocks19@imperial.ac.uk>
2023-04-20 19:58:55 +0100

    Start type3 clone detection

--- src/main/kotlin/refactor/refactorings/removeDuplication/common/ProcessedMethod.kt
+++ src/main/kotlin/refactor/refactorings/removeDuplication/common/ProcessedMethod.kt
@@ -1,8 +1,8 @@
-package refactor.refactorings.extractType1Clones.methodProcessors
+package refactor.refactorings.removeDuplication.common
 
 import com.github.javaparser.ast.body.MethodDeclaration
 
 class ProcessedMethod(val method: MethodDeclaration) {
-    val normalisedMethod = Normaliser().normaliseMethod(method)
+    val normalisedMethod = MethodNormaliser().normalise(method)
     val metrics = MethodMetrics().process(normalisedMethod)
 }
