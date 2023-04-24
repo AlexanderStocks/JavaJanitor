90cc17d3b8c40146691e7239e55c3717ea747ef1
AlexStocks <alex.stocks19@imperial.ac.uk>
2023-04-24 23:30:15 +0100

    Update ktor to 2.3.0

--- src/main/kotlin/github/GithubAPI.kt
+++ src/main/kotlin/github/GithubAPI.kt
@@ -8,11 +8,13 @@ import github.apiFormats.Branch
 import github.apiFormats.RepositoryContents
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.call.body
 import io.ktor.client.engine.apache.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.http.content.*
+import io.ktor.server.http.content.*
 import kotlinx.coroutines.runBlocking
 import utils.Utils
 import java.io.File
