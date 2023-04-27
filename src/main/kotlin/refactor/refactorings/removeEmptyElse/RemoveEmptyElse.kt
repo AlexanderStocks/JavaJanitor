0a7ef26298d6f0b091a6240ab7706ff7a68cb504
AlexStocks <alex.stocks19@imperial.ac.uk>
2023-04-27 18:15:41 +0100

    RemoveEmptyElse works with javaparser

---
+++ src/main/kotlin/refactor/refactorings/removeEmptyElse/RemoveEmptyElse.kt
@@ -0,0 +1,28 @@
+package refactor.refactorings
+
+import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.stmt.IfStmt
+import refactor.Refactoring
+import java.nio.file.Path
+
+class RemoveEmptyElse : Refactoring {
+    override fun process(cus: List<CompilationUnit>): List<Path> {
+        return cus.mapNotNull { removeEmptyElseBlocksAndSave(it) }
+    }
+
+    private fun removeEmptyElseBlocksAndSave(cu: CompilationUnit): Path? {
+        val originalCuString = cu.toString()
+        removeEmptyElseBlocks(cu)
+        return if (originalCuString != cu.toString()) cu.storage.get().path else null
+    }
+
+    private fun removeEmptyElseBlocks(cu: CompilationUnit) {
+        cu.findAll(IfStmt::class.java).forEach { ifStmt ->
+            ifStmt.elseStmt.ifPresentOrElse({ elseStmt ->
+                if (elseStmt.isBlockStmt && elseStmt.asBlockStmt().statements.isEmpty()) {
+                    ifStmt.removeElseStmt()
+                }
+            }, { /* Do nothing if there's no else statement */ })
+        }
+    }
+}
